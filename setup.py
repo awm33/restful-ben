49e697c7bef172f24ce606f4767a010f08897f3c
Andrew Madonna <andrew.w.madonna@gmail.com>
2017-07-01 14:29:57 -0400

    Update requirements

--- setup.py
+++ setup.py
@@ -8,7 +8,9 @@ setup(
     version='0.0.1',
     packages=find_packages(),
     install_requires=[
+        'argon2-cffi==16.3.0',
         'Flask==0.12.2',
+        'Flask-Login==0.4.0',
         'Flask-RESTful==0.3.6',
         'Flask-SQLAlchemy==2.2',
         'marshmallow==2.13.5',
