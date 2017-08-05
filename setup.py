e330b8b1359f94112614a8812fddf09c687ee290
Andrew Madonna <andrew.w.madonna@gmail.com>
2017-08-05 14:35:59 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name='restful_ben',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     install_requires=[
         'argon2-cffi==16.3.0',
